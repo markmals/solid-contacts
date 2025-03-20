f2e861780bb124030c38a55b6f4fd7d83f0453c8
Mark Malstrom <mark@malstrom.me>
2025-03-20 15:07:27 -0500

    Add index route

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+export default function Component() {
+    return (
+        <p id="zero-state">
+            This is a demo for Solid and SolidStart.
+            <br />
+            Check out{" "}
+            <a href="https://docs.solidjs.com/" target="_blank">
+                the docs at solidjs.com
+            </a>
+            .
+        </p>
+    );
+}
