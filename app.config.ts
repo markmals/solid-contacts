317013e09fbf4f54deca37440bc14c54dfbb0e08
Mark Malstrom <mark@malstrom.me>
2025-03-20 13:22:41 -0500

    Mark @deno/kv as external

--- app.config.ts
+++ app.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
     ssr: true,
-    // vite: {
-    //     ssr: {
-    //         external: ["@deno/kv"],
-    //     },
-    // },
+    vite: {
+        ssr: {
+            external: ["@deno/kv"],
+        },
+    },
 });
